638f6b0358965306c12e2057e9d4365d7698c1a0
Kabish <kabishsridar6@gmail.com>
2025-05-22 11:49:35 +0530

    created

---
+++ ex_3.py
@@ -0,0 +1,3 @@
+quote = input("What is the Quote? ") # Get the quote from the user
+name = input("Who said it? ") # get the name of autor
+print(f"{name} says, \"{quote}\"") # display those with double quotes as i used escape sequence here
