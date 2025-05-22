42a8435c3c21d977aafa8559a7dd4945cf1e7b1c
Kabish <kabishsridar6@gmail.com>
2025-05-22 11:23:09 +0530

    create ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,3 +1,3 @@
-data = input("What is the input string? ")
-count = len(data)
-print(f"{data} has {count} character")
+data = input("What is the input string? ") # Get the input from the user for the string to count the number of characters and store it as data
+count = len(data) # calculte the length of data and store it as count
+print(f"{data} has {count} character") # dispay the output
