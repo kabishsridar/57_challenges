65e0610e8cf5bab677fd1627aacb0f21b0fbb56c
Kabish <kabishsridar6@gmail.com>
2025-05-22 13:13:42 +0530

    created

---
+++ ex_5.py
@@ -0,0 +1,6 @@
+first_number = int(input("Enter first number: ")) # prompt user for first number
+second_number = int(input("Enter second number: ")) # prompt user for second number
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} * {second_number} = {first_number*second_number}")
+print(f"{first_number} / {second_number} = {first_number/second_number}")
