0bc6308276e410248760def38b78d4717b168923
Kabish <kabishsridar6@gmail.com>
2025-05-22 21:51:32 +0530

    created

--- git_folder_python.py
+++ git_folder_python.py
@@ -1,19 +1,18 @@
-import os
-import shutil
+import os # importing os module for path function and makedirs
+import shutil # importing shutil module to move the file to dedicated path
 
 # Define the files to move
-files_to_move = ["ex_4.py", "ex_4_notes.txt"]
+files1 = ["ex_8_notes.txt", "ex_8.py", "ex_8_ch1.py","ex_8_ch1_notes.txt", "ex_8_ch2.py", "ex_8_ch", "ex_8_ch2_notes.txt", ""]
 
 # Define the target folder
-target_folder = "ex_4_folder"
-
+target_folder2 = "ex_8_folder"
 # Create the target folder if it doesn't exist
-os.makedirs(target_folder, exist_ok=True)
-
+os.makedirs(target_folder2, exist_ok=True)
 # Move each file to the target folder
-for file in files_to_move:
+
+for file in files1:
     if os.path.exists(file):
-        shutil.move(file, os.path.join(target_folder, file))
-        print(f"Moved {file} to {target_folder}/")
+        shutil.move(file, os.path.join(target_folder2, file))
+        print(f"Moved {file} to {target_folder2}/")
     else:
         print(f"File not found: {file} ")
