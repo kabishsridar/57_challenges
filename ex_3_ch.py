05c196ea7bbb1926075eb1b9cf2407d2cdf051d1
Kabish <kabishsridar6@gmail.com>
2025-05-22 12:24:07 +0530

    created

---
+++ ex_3_ch.py
@@ -0,0 +1,4 @@
+lst = [['George Eliot','It is never too late to be what you might have been.'],['Obi-Wan Kenobi',"These aren't the droids you're looking for."],['Haruki Murakami','Pain is inevitable. Suffering is optional.']]
+
+for i in lst:
+    print(i[0] + ' says, "' + i[1] + '"')
