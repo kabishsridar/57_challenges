0abe51d9d7c9e8c145fa237d6e3bc0fe8a24be3d
Kabish <kabishsridar6@gmail.com>
2025-05-23 13:34:51 +0530

    created

--- ex_10.py
+++ ex_10.py
@@ -1,11 +1,11 @@
-subtotal = 0
-for loop in range(3):
-    price = float(input(f"Enter the price of item {loop + 1}: "))
-    quantity = float(input(f"Enter the quantity of item {loop + 1}: "))
-    item_total = price * quantity
-    subtotal += item_total
-    tax = subtotal * 0.055
-    total = tax + subtotal
-print(f"SubTotal: ${subtotal}")
+subtotal = 0 # initiating subtotal as 0
+for loop in range(3): # iterate 3 times to get price for 3 items
+    price = float(input(f"Enter the price of item {loop + 1}: ")) # prompt user for price of item
+    quantity = float(input(f"Enter the quantity of item {loop + 1}: ")) # prompt usre for number of items
+    item_total = price * quantity # item_total will be the complete price of item
+    subtotal += item_total # subtotal will be the total for all items
+    tax = subtotal * 0.055 # tax rate mentioned 5.5% so multiplied by 0.55 to get tax
+    total = tax + subtotal # grand total
+print(f"SubTotal: ${subtotal}") # displaying output
 print(f"Tax: ${tax}")
 print(f"Total: ${total}")
