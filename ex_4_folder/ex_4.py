29990c2fc58682211622e7cf3ffdbe732224e881
Kabish <kabishsridar6@gmail.com>
2025-05-24 11:20:44 +0530

    updated comments

--- ex_4_folder/ex_4.py
+++ ex_4_folder/ex_4.py
@@ -1,5 +1,5 @@
-noun = input("Enter a noun: ")
-verb = input("Enter a verb: ")
-adjective = input("Enter an adjective: ")
-adverb = input("Enter an adverb: ")
-print(f"do you {verb} your {adjective} {noun} {adverb}? That's hilarious! ")
+noun = input("Enter a noun: ") # prompt the user for noun in the sentence
+verb = input("Enter a verb: ") # prompt the user for verb in the sentence
+adjective = input("Enter an adjective: ") # prompt the adjective for noun in the sentence
+adverb = input("Enter an adverb: ") # prompt the user for adverb in the sentence
+print(f"do you {verb} your {adjective} {noun} {adverb}? That's hilarious! ") # display the output with correct format
