01692fae25ed2697f689a94b5556f971bc815c56
Kabish <kabishsridar6@gmail.com>
2025-05-24 21:28:40 +0530

    created

---
+++ ex_15.py
@@ -0,0 +1,7 @@
+username = input("Enter the username: ")
+passwd = input("What is your password?  ")
+password = "abc$123"
+if passwd == password:
+    print("Welcome!")
+else:
+    print("I dont know you")
