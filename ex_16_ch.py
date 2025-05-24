0537daa35ec3db49672918f23a3ddae04e9cbaf5
Kabish <kabishsridar6@gmail.com>
2025-05-24 22:43:00 +0530

    updated dictionary

--- ex_16_ch.py
+++ ex_16_ch.py
@@ -5,13 +5,14 @@ driving_age = { # just to store the driving ages of various country
     "CA" : 16,
     "MY" : 17
 }
-if age >= 18:
-    print("You are eligible to drive in IND")
-elif age >= 17:
-    print("You are eligible to drive in MY")
 
-elif age >= 16:
-    print("You are eligible to drive in US and CA")
+print("You are old enough to legally drive." if age >= 16 else "You are not old enough to legally drive.")
+eligible_countries = []
+for country, minage in driving_age.items():
+    if age >= minage:
+        eligible_countries.append(country)
 
+if eligible_countries != []:
+        print("Based on your age, you can legally drive in: " + ", ".join(eligible_countries))
 else:
-    print("Enter a valid age")
+        print("Based on your age, you cannot legally drive in any of the listed countries.")
