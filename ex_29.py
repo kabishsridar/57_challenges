2fa0f0a759e6a9563a89f3a4e720a4c86084a1fc
Kabish <kabishsridar6@gmail.com>
2025-05-25 17:51:36 +0530

    created

---
+++ ex_29.py
@@ -0,0 +1,12 @@
+while True: # loop until it breaks
+    rate = input("What is the rate of return? ") # prompt the user for rate
+    if rate == "0": # if it is 0 or non digit display error message
+        print("Sorry, that's not a valid input")
+    elif not rate.isdigit():
+        print("Sorry, that's not a valid input")
+    else: # if it is a digit, convert to integer, and break out of function
+        rate = int(rate)
+        break
+
+years = 72/rate # calculate years by given formula
+print(f"It will take {years} years to double your initial investment") # display the output
