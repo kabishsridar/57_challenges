63bd2dd6ff575348982aa5eb72eb90f284d67d2c
Kabish <kabishsridar6@gmail.com>
2025-05-25 10:42:26 +0530

    created

--- ex_16_folder/ex_16_ch.py
+++ ex_16_folder/ex_16_ch.py
@@ -7,12 +7,12 @@ driving_age = { # just to store the driving ages of various country
 }
 
 print("You are old enough to legally drive." if age >= 16 else "You are not old enough to legally drive.")
-eligible_countries = []
-for country, minage in driving_age.items():
-    if age >= minage:
-        eligible_countries.append(country)
+eligible_countries = [] # initiate eligible_countries as empty list
+for country, minage in driving_age.items(): # loop through each items in dictionary
+    if age >= minage: # if age is greater than or equal to the minage in dictionary
+        eligible_countries.append(country) # add the country to eligible countries
 
-if eligible_countries != []:
+if eligible_countries != []: # check if it is not empty and display the eligible countries
         print("Based on your age, you can legally drive in: " + ", ".join(eligible_countries))
-else:
+else: # if it is empty, diplay you cannot legally drive
         print("Based on your age, you cannot legally drive in any of the listed countries.")
---
+++ folder_movement.py
@@ -0,0 +1,44 @@
+# requirement: user will give exercise name, example ex_1, the files having this name must be moved to the folder
+# with the name ex_1
+# get the folder name as an input and store as target folder
+# assign files1, files2, etc as the file names
+# make those as a list
+# define a function named move_folder
+# use the input given by the user and search for the files starting with the target folder
+# if the target folder already exists, add the file to the respective folder, else create a new folder and add the files to it.
+# use os module to find whether the file exists or not
+# move the file to respective folder using shutil
+
+import os # importing os module for path function and makedirs
+import shutil # importing shutil module to move the file to dedicated path
+
+#target_folder = input("Enter the folder name: ")
+
+
+
+# print(os.listdir("."))
+
+# Move each file to the target folder
+# dict1 = {"name":"os", "functions": []}
+# print(f"list names: {list_filenames}")
+
+
+def move_file_to_folder(foldername):
+    list_filenames = os.listdir(".") # it will include also the folders
+    os.mkdir(foldername)
+    for fname in list_filenames:
+        if os.path.isfile(fname):
+            if fname.startswith(foldername):
+                # print(f"the file name is {fname} and folder is {foldername}")
+                shutil.move(fname, os.path.join(foldername, fname))
+                print(f"Moved {fname} to {foldername}/")
+        else:
+            print(f"{fname} is a folder , not moving")
+
+# move_file_to_folder("ex_18")
+
+if __name__ == "__main__":
+    for folder in range(5):
+        foldername = f"ex_{folder+19}"
+        print(foldername)
+        # move_file_to_folder(foldername)
