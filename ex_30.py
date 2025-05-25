44f855afcf3a51ba2bb96eb53e0575c957c3eb9c
Kabish <kabishsridar6@gmail.com>
2025-05-25 19:05:50 +0530

    upated function

--- ex_30.py
+++ ex_30.py
@@ -1,3 +1,7 @@
-for i in range(13): # loops upto 12
-    for j in range(13): # loops upto 12
-        print(f"{i} X {j} = {i * j}")
+def multiply_table(n): # defining a function named multiply_table
+    for i in range(n + 1): # loop for maximum number entered by user (n + 1 is used as for loop starts from 0)
+        for j in range(n + 1):
+            print(f"{i} X {j} = {i * j}") # displaying the output
+if __name__ == "__main__":
+    n = int(input("Enter maximum number of multiplication table(from 2 to 200): "))
+    multiply_table(n)
