857fc161b350eb54d6f2a038aca0d969d706fc2c
Kabish <kabishsridar6@gmail.com>
2025-05-25 17:24:44 +0530

    created

---
+++ ex_28.py
@@ -0,0 +1,5 @@
+total = 0
+for number in range(5): # loop for 5 numbers
+    num = int(input("Enter a number: ")) # prompt the number
+    total += num # adds total 
+print(f"The total is {total}") # display the total
