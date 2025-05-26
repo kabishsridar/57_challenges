9d231e09131af0bc4484bc4ee2308641f66fc8de
Kabish <kabishsridar6@gmail.com>
2025-05-26 10:00:37 +0530

    created

--- ex_33.py
+++ ex_33.py
@@ -1,9 +1,9 @@
-import random
-def answers():
-    choices_lst =["Yes", "No", "Maybe", "Ask"]
-    choice = []
-    n = random.randint(1,4)
-    if n == 1:
+import random # import random module for answers
+def answers(): # define a function named answers to choose answers
+    choices_lst =["Yes", "No", "Maybe", "Ask"] # initiate choice lst as the poosible answers list from the question
+    choice = [] # initiate an empty list choice
+    n = random.randint(1,4) # use random module to select the numbers between 1 to 4
+    if n == 1: # each numbers represent each answer
         choice = choices_lst[0]
     elif n == 2:
         choice = choices_lst[1]
@@ -11,7 +11,7 @@ def answers():
         choice = choices_lst[2]
     elif n == 4:
         choice = choices_lst[3]
-    print(choice)
-que = input("What's you question? ")
+    print(choice) # display the choice in last of the function
+que = input("What's your question? ") # prompt the user for question from user
 if __name__ == "__main__":
     answers()
