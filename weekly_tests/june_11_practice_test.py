4d028f0d7e4b82d2045be7021bafe3e0b6c0b08d
Kabish <kabishsridar6@gmail.com>
2025-06-11 09:48:49 +0530

    completed problem

--- weekly_tests/june_11_practice_test.py
+++ weekly_tests/june_11_practice_test.py
@@ -1,13 +1,13 @@
-inp = input("address: ")
-def defanging(inp):
-    result = []
-    for char in inp:
-        if char != ".":
+inp = input("address: ") # prompt the user for the address
+def defanging(inp): # defining a function to defang
+    result = [] # initiating result as an empty list
+    for char in inp: # loop through each character in the inp
+        if char != ".": # if the character is not a . then append directly
             result.append(char)
-        if char == ".":
+        if char == ".": # else, append after adding list brackets
             char = "[.]"
             result.append(char)
 
     for char in result:
-        print(char, end="")
-defanging(inp)
+        print(char, end="") # to display the list in format of string for legible
+defanging(inp) # calling the function
