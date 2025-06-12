33004d9a7b72f70b417c8d396efb307e6fc1578d
Kabish <kabishsridar6@gmail.com>
2025-06-12 16:18:03 +0530

    updated comments

--- weekly_tests/june_12_practice_test.py
+++ weekly_tests/june_12_practice_test.py
@@ -20,31 +20,31 @@ def plot_average(filename):
     category_sums = {} # initiating empty dictionaries
     category_counts = {}
 
-    for data in lst1:
+    for data in lst1: # loop through each categories
         current_category = data[1]
-        current_amount = float(data[2])
+        current_amount = float(data[2]) # converting amount to float
 
         if current_category in category_sums:
-            category_sums[current_category] += current_amount
-            category_counts[current_category] += 1
+            category_sums[current_category] += current_amount # adding amount to existing sum
+            category_counts[current_category] += 1 # incrementing the count
         else:
-            category_sums[current_category] = current_amount
+            category_sums[current_category] = current_amount  # do not increment
             category_counts[current_category] = 1
 
-    category_list = []
-    average_list = []
+    category_list = [] # initiating category_list as an empty list
+    average_list = [] # initiating average_list as an empty list
 
-    for category in distinct_categories:
+    for category in distinct_categories: # loop through distinct categories
         if category in category_sums and category_counts[category] > 0:
-            calculated_average = category_sums[category] / category_counts[category]
-            category_list.append(category)
-            average_list.append(calculated_average)
+            calculated_average = category_sums[category] / category_counts[category]  # calculating average
+            category_list.append(category) # appending category to list
+            average_list.append(calculated_average) # appending calculated average
 
-    plt.bar(category_list, average_list)
-    plt.title('Average amounts of each categories:')
-    plt.show()
-if __name__ == "__main__":
+    plt.bar(category_list, average_list) # plots the bar graph of averages for each category
+    plt.title('Average amounts of each categories:') 
+    plt.show() # displays the graph
+if __name__ == "__main__": # this should execute at first
     print(type(argv))
     print(f"script: {argv[0]}")
-    print(f"input file: {argv[1]}")
-    plot_average(argv[1])
+    print(f"input file: {argv[1]}") # in the command prompt we need to run the file with the syntax python june_12_practice_test.py sample_expenses.csv
+    plot_average(argv[1]) # calling the function
